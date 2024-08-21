4322668162289e90b4785b0b7912680caa6a3425
Katie Wan <katiewan.kw@gmail.com>
2024-08-21 13:20:27 +0100

    Set up tailwind

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,18 @@
+import logo from "./logo.jpg";
+
+export const assets = {
+  logo,
+};
+
+export const products = [
+  {
+    _id: "aaaaa",
+    name: "GCSE notes",
+    description: "Grade 9",
+    price: 1,
+    image: [],
+    category: "GCSE",
+    date: 2023,
+    bestseller: true,
+  },
+];
