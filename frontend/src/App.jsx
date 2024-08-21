b2a5b7046c97f9455c8ff4b4a0437cf8e9f83811
Katie Wan <katiewan.kw@gmail.com>
2024-08-21 13:30:18 +0100

    Create pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Orders from "./pages/Orders";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="px-4">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/prders" element={<Orders />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
