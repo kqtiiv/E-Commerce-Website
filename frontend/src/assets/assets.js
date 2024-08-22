8d613d2d1db9d6215b616d0cc0269382d15ac535
Katie Wan <katiewan.kw@gmail.com>
2024-08-22 18:28:35 +0100

    Add products

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -7,12 +7,24 @@ export const assets = {
 export const products = [
   {
     _id: "aaaaa",
-    name: "GCSE notes",
-    description: "Grade 9",
-    price: 1,
+    name: "GCSE Computer Science OCR J277 - Notes for Paper 1",
+    description: `Grade: 9
+      Specification: OCR J277
+      Type: Detailed, full set of notes for paper 1`,
+    price: 5,
     image: [],
     category: "gcse",
-    date: 2023,
+    bestseller: true,
+  },
+  {
+    _id: "aaaab",
+    name: "GCSE English Literature Macbeth Notes",
+    description: `Grade: 9
+      Specification: Edexcel
+      Type: Detailed, full set of notes for Macbeth`,
+    price: 6,
+    image: [],
+    category: "gcse",
     bestseller: true,
   },
 ];
